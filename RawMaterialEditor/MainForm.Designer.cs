1160cdc1e532e225469d7b1db0515af3b62cc47a
BiscuitDev <86505354+Boxofbiscuits97@users.noreply.github.com>
2025-02-12 17:43:06 -0500

    Darkmode & Material Variable Names

--- RawMaterialEditor/MainForm.Designer.cs
+++ RawMaterialEditor/MainForm.Designer.cs
@@ -20,80 +20,91 @@
             base.Dispose(disposing);
         }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			propertyGrid = new PropertyGrid();
-			btnOpen = new Button();
-			btnSave = new Button();
-			openFileDialog = new OpenFileDialog();
-			saveFileDialog = new SaveFileDialog();
-			SuspendLayout();
-			// 
-			// propertyGrid
-			// 
-			propertyGrid.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-			propertyGrid.BackColor = SystemColors.Control;
-			propertyGrid.Location = new Point(12, 12);
-			propertyGrid.Name = "propertyGrid";
-			propertyGrid.PropertySort = PropertySort.Categorized;
-			propertyGrid.Size = new Size(460, 308);
-			propertyGrid.TabIndex = 0;
-			propertyGrid.ToolbarVisible = false;
-			// 
-			// btnOpen
-			// 
-			btnOpen.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-			btnOpen.Location = new Point(12, 326);
-			btnOpen.Name = "btnOpen";
-			btnOpen.Size = new Size(75, 23);
-			btnOpen.TabIndex = 1;
-			btnOpen.Text = "Open...";
-			btnOpen.UseVisualStyleBackColor = true;
-			btnOpen.Click += btnOpen_Click;
-			// 
-			// btnSave
-			// 
-			btnSave.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-			btnSave.Enabled = false;
-			btnSave.Location = new Point(397, 326);
-			btnSave.Name = "btnSave";
-			btnSave.Size = new Size(75, 23);
-			btnSave.TabIndex = 2;
-			btnSave.Text = "Save...";
-			btnSave.UseVisualStyleBackColor = true;
-			btnSave.Click += btnSave_Click;
-			// 
-			// openFileDialog
-			// 
-			openFileDialog.Filter = "Raw material file|*.material";
-			// 
-			// saveFileDialog
-			// 
-			saveFileDialog.Filter = "Raw material file|*.material";
-			// 
-			// MainForm
-			// 
-			AutoScaleDimensions = new SizeF(7F, 15F);
-			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(484, 361);
-			Controls.Add(btnSave);
-			Controls.Add(btnOpen);
-			Controls.Add(propertyGrid);
-			Name = "MainForm";
-			StartPosition = FormStartPosition.CenterScreen;
-			Text = "HD2 Raw Material Editor";
-			ResumeLayout(false);
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            propertyGrid = new PropertyGrid();
+            btnOpen = new Button();
+            btnSave = new Button();
+            openFileDialog = new OpenFileDialog();
+            saveFileDialog = new SaveFileDialog();
+            SuspendLayout();
+            // 
+            // propertyGrid
+            // 
+            propertyGrid.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            propertyGrid.BackColor = Color.FromArgb(64, 64, 64);
+            propertyGrid.CategorySplitterColor = Color.Silver;
+            propertyGrid.CommandsBackColor = Color.FromArgb(64, 64, 64);
+            propertyGrid.CommandsBorderColor = Color.FromArgb(64, 64, 64);
+            propertyGrid.HelpBackColor = Color.Silver;
+            propertyGrid.HelpBorderColor = Color.FromArgb(64, 64, 64);
+            propertyGrid.LineColor = Color.Silver;
+            propertyGrid.Location = new Point(12, 12);
+            propertyGrid.Name = "propertyGrid";
+            propertyGrid.PropertySort = PropertySort.Categorized;
+            propertyGrid.SelectedItemWithFocusForeColor = Color.FromArgb(224, 224, 224);
+            propertyGrid.Size = new Size(460, 308);
+            propertyGrid.TabIndex = 0;
+            propertyGrid.ToolbarVisible = false;
+            propertyGrid.ViewBackColor = Color.Silver;
+            propertyGrid.ViewBorderColor = Color.FromArgb(64, 64, 64);
+            propertyGrid.Click += propertyGrid_Click;
+            // 
+            // btnOpen
+            // 
+            btnOpen.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnOpen.Location = new Point(12, 326);
+            btnOpen.Name = "btnOpen";
+            btnOpen.Size = new Size(75, 23);
+            btnOpen.TabIndex = 1;
+            btnOpen.Text = "Open...";
+            btnOpen.UseVisualStyleBackColor = true;
+            btnOpen.Click += btnOpen_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnSave.Enabled = false;
+            btnSave.Location = new Point(397, 326);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(75, 23);
+            btnSave.TabIndex = 2;
+            btnSave.Text = "Save...";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // openFileDialog
+            // 
+            openFileDialog.Filter = "Raw material file|*.material";
+            // 
+            // saveFileDialog
+            // 
+            saveFileDialog.Filter = "Raw material file|*.material";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 64);
+            ClientSize = new Size(484, 361);
+            Controls.Add(btnSave);
+            Controls.Add(btnOpen);
+            Controls.Add(propertyGrid);
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "HD2 Raw Material Editor";
+            ResumeLayout(false);
+        }
 
-		#endregion
+        #endregion
 
-		private PropertyGrid propertyGrid;
+        private PropertyGrid propertyGrid;
 		private Button btnOpen;
 		private Button btnSave;
 		private OpenFileDialog openFileDialog;
--- RawMaterialEditor/MainForm.cs
+++ RawMaterialEditor/MainForm.cs
@@ -2,29 +2,34 @@ namespace RawMaterialEditor;
 
 public partial class MainForm : Form
 {
-	public MainForm()
-	{
-		InitializeComponent();
-	}
+    public MainForm()
+    {
+        InitializeComponent();
+    }
 
-	private void btnOpen_Click(object sender, EventArgs e)
-	{
-		if (openFileDialog.ShowDialog(this) == DialogResult.OK)
-		{
-			using var stream = File.OpenRead(openFileDialog.FileName);
-			using var reader = new BinaryReader(stream);
-			propertyGrid.SelectedObject = MaterialResource.Deserialize(reader);
-			btnSave.Enabled = true;
-		}
-	}
+    private void btnOpen_Click(object sender, EventArgs e)
+    {
+        if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+        {
+            using var stream = File.OpenRead(openFileDialog.FileName);
+            using var reader = new BinaryReader(stream);
+            propertyGrid.SelectedObject = MaterialResource.Deserialize(reader);
+            btnSave.Enabled = true;
+        }
+    }
 
-	private void btnSave_Click(object sender, EventArgs e)
-	{
-		if (saveFileDialog.ShowDialog(this) == DialogResult.OK)
-		{
-			using var stream = File.OpenWrite(saveFileDialog.FileName);
-			using var writer = new BinaryWriter(stream);
-			((MaterialResource)propertyGrid.SelectedObject!).Serialize(writer);
-		}
-	}
+    private void btnSave_Click(object sender, EventArgs e)
+    {
+        if (saveFileDialog.ShowDialog(this) == DialogResult.OK)
+        {
+            using var stream = File.OpenWrite(saveFileDialog.FileName);
+            using var writer = new BinaryWriter(stream);
+            ((MaterialResource)propertyGrid.SelectedObject!).Serialize(writer);
+        }
+    }
+
+    private void propertyGrid_Click(object sender, EventArgs e)
+    {
+
+    }
 }
